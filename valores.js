7c11589746d6245157a4ba9d8e5eb64c115ca72b
edumoyano86 <cba2486@gmail.com>
2021-12-01 14:04:17 -0300

    Aprendiendo JS

---
+++ valores.js
@@ -0,0 +1,14 @@
+40
+"Eduardo Moyano"
+true
+false     // Boolean
+
+null
+undefined  // valores vacios, valores reservados para un tipo de valor... generan un espacio reservado
+
+
+// todos estos valores anteriores se los conocen como valores primitivos, porque son los valores basicos
+
+
+[1,2,3];  // tipo objeto
+{ nombre: Eduardo} // valor tipo objeto
---
+++ variables.js
@@ -0,0 +1,15 @@
+var nombre = "Eduardo";
+
+// declarar
+var edad;
+
+// inicializar 
+edad = 35;
+
+var elementos = ["Computadora", "Celular"];
+
+var persona = {
+    nombre: "Eduardo",
+    edad: 30
+}
+
